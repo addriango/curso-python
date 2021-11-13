4818e591f6fd0a841cc29ad0e0d37d048007ab89
Adrian Gomez <51376902+adriangomezriv@users.noreply.github.com>
2021-11-12 20:34:15 -0500

    aguegue algunas lineas nuevas

--- sintaxis-basica/02.operadores.py
+++ sintaxis-basica/02.operadores.py
@@ -1,86 +1,83 @@
 #Operadores arimeticos/ matematicos
 
-## suma
-+
+## suma+
 print(2+2) #4
-## resta
--
+
+## resta-
 print(4-3) #1
-## multiplicacion
-*
+
+## multiplicacion*
 print(3*3) #9
-## division con decimal
-/
+
+## division con decimal/
 print(10/3) #3.33333
-## division entera
-//
+
+## division entera//
 print(10//3) #3
-## exponente
-**
+
+## exponente**
 print(2**3) #8
-## modulo
-%
+
+## modulo%
 print(10%3) #1
 
 
 #Operadores de comaracion
 
-## igual que
-==
+## igual que ==
 5 == 5
-## Mayor que
->
+
+## Mayor que >
 6 > 2
-## menor que
-<
+
+## menor que <
 4 < 8
-## diferente que
-!=
+
+## diferente que !=
 3 != 5
-## mayor o igual que
->=
+
+## mayor o igual que >=
 3 >= 3   #si
 3 >= 4   #si
-## menor o igual que
-<=
+
+## menor o igual que <=
 5<=5 #si
 5<=7 #si
+
 # Operadores Logicos
 
-## operedor Y (las dos condiciones de cumplen)
-AND
+## operedor Y (las dos condiciones de cumplen)AND
 1 < 3 & 4 < 6 #si
 "edad" == "edad" & "nombre" == "nombre"
-## operador O (una de las dos condiciones se cumple)
-OR
+
+## operador O (una de las dos condiciones se cumple)OR
 "negro"=="negro" | "negro"== "amarillo"
+
 ##Operador not
 NOT
 
 #Operadores de asignacion
 
-## igual 
-=
+## igual =
 nombre = "pedro"
 edad = 18
 es_casado = True
-##incremento
-+=
+
+##incremento +=
 edad += 1
-##decremento
--=
+
+##decremento -=
 edad -=2
-## otros asociadas a cada operacion matematica
-*=
+
+## otros asociadas a cada operacion matematica *=
 edad *=2
-/=
-%=
-**=
-//=
+# /=
+# %=
+# **=
+# //=
 
-#Operadores especiales
-
-IS
-IN
-IS NOT
-NOT IN
+# Operadores especiales
+# IS
+# IN
+# IS NOT
+# NOT IN
